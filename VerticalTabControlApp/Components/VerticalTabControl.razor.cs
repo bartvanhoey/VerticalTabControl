bf6f7f40cbd9b8052d9b4b2a47983a45977e1f06
ekapituro <ekapituro@gmail.com>
2025-03-01 19:48:32 +0100

    Update

---
+++ VerticalTabControlApp/Components/VerticalTabControl.razor.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace VerticalTabControlApp.Components;
+
+public class VerticalTabControlBase : ComponentBase
+{
+[Parameter] public RenderFragment? ChildContent { get; set; }
+
+public TabPage? ActivePage { get; private set; }
+protected readonly List<TabPage?> Pages = [];
+
+internal void AddPage(TabPage? tabPage)
+{
+    Pages.Add(tabPage);
+    if (Pages.Count == 1)
+        ActivePage = tabPage;
+    StateHasChanged();
+}
+
+protected string GetActiveClass(TabPage? page) => page == ActivePage ? "active" : "";
+
+protected void ActivatePage(TabPage? page)
+{
+    ActivePage = page;
+    StateHasChanged();
+}
+
+}
